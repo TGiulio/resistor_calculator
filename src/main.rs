c2d2c65828ae0cff99563498ffc554c9d315320d
Giulio Toldo <giulio.toldo@gmail.com>
2023-05-01 17:42:03 +0200

    add some tests, use f32 everywhere

--- src/main.rs
+++ src/main.rs
@@ -1,6 +1,6 @@
 use std::io;
 fn main() {
-    let mut resistors:Vec<u64> = Vec::new();
+    let mut resistors:Vec<f32> = Vec::new();
     while resistors.len() == 0 {
         let mut list: String = String::new();
         println!("write a list of resistor values separated by a space");
@@ -8,7 +8,7 @@ fn main() {
             Ok(_size) => {
                 let v: Vec<&str> = list.split_whitespace().collect();
                 for n in v{
-                    match n.parse::<u64>() {
+                    match n.parse::<f32>() {
                         Ok(num) => {
                             resistors.push(num);
                         }
@@ -26,12 +26,24 @@ fn main() {
     println!("The parallel of {:?} is {} Ω", resistors, calculate_parallel(&resistors));
 }
 
-fn calculate_parallel(resistors: &Vec<u64>) -> f64 {
-    let mut denominator: f64 = 0.0;
+fn calculate_parallel(resistors: &Vec<f32>) -> f32 {
+    let mut denominator: f32  = 0.0;
     for res in resistors {
-        denominator += 1.0/(*res as f64) ;
+        denominator += 1.0/(*res ) ;
     }
     1.0/denominator
 }
 
+fn calculate_series(resistors: &Vec<f32>) -> f32 {
+    resistors.iter().fold(0.0, |acc, x| acc + x)
+}
 
+#[test]
+fn test_parallel() -> (){
+    assert_eq!(calculate_parallel(&vec![100.0, 100.0]), 50.0);
+}
+
+#[test]
+fn test_series() -> (){
+    assert_eq!(calculate_series(&vec![100.0, 100.0]), 200.0)
+}
